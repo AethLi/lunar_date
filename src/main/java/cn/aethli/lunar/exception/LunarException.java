aff8ecf90ab18d22967d21074d31dc30ab261ffe
YRJ <931622815@qq.com>
2019-12-09 17:00:46 +0800

    wait for test

---
+++ src/main/java/cn/aethli/lunar/exception/LunarException.java
@@ -0,0 +1,29 @@
+package cn.aethli.lunar.exception;
+
+/**
+ * @author Termite
+ * @device Hades
+ * @date 2019-12-09 10:27
+ **/
+public class LunarException extends Exception {
+
+  public LunarException() {
+  }
+
+  public LunarException(String message) {
+    super(message);
+  }
+
+  public LunarException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public LunarException(Throwable cause) {
+    super(cause);
+  }
+
+  public LunarException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
